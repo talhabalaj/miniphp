1229938899dd92317d7f57b003120b1b5d561312
Talha Balaj <talhabalaj@hotmail.com>
2021-02-13 17:08:24 +0500

    update

--- helpers.h
+++ helpers.h
@@ -1,11 +1,11 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char true_string[5] = "true";
 char false_string[6] = "false";
 
-char* boolean_to_string(int boolean) {
+char *boolean_to_string(int boolean) {
   if (boolean) {
     return true_string;
   } else {
@@ -13,8 +13,8 @@ char* boolean_to_string(int boolean) {
   }
 }
 
-char* int_to_string(int value) {
-  char* str = (char*)malloc(sizeof(char)*25);
+char *int_to_string(int value) {
+  char *str = (char *)malloc(sizeof(char) * 25);
   sprintf(str, "%d", value);
   return str;
 }
@@ -25,14 +25,18 @@ char *float_to_string(int value) {
   return str;
 }
 
-char* read_string() {
+char *read_string() {
   size_t bufsize = 100;
-  char* buffer = (char *)malloc(bufsize * sizeof(char));
+  char *buffer = (char *)malloc(bufsize * sizeof(char));
   size_t characters = getline(&buffer, &bufsize, stdin);
   buffer = realloc(buffer, characters);
   return buffer;
 }
 
-char* get_query_string() {
-  return strdup(getenv("QUERY_STRING"));
+char *get_query_string() {
+  char *str = getenv("QUERY_STRING");
+  if (str != NULL)
+    return str;
+  else
+    return strdup("null");
 }
